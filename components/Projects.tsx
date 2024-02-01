ad9331cf30312fbc22f3289d0bacdc2700e0794f
tugrul95 <tugrul1995@gmail.com>
2024-02-01 14:14:08 +0300

    add new projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -14,90 +14,64 @@ const Projects = () => {
       <div className="container mx-auto 2xl">
         <div className="flex-col flex md:flex-row mt-7">
           {/* Scribble School App */}
-          <Link
-            href="https://scribble-school.netlify.app/"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="z-[1]"
-          >
-            <div className="flex-row flex mb-5">
-              <Image
-                src="/scribble-school.png"
-                height={150}
-                width={150}
-                alt="WebHR Project"
-              />
-              <div className="p-3">
-                <p className="text-white font-semibold text-xl">Scribble School App</p>
-              </div>
-            </div>
-          </Link>
+          {/* ... (unchanged) */}
 
           {/* VIP Clinic */}
-          <Link
-            href="https://vip-clinic.netlify.app/"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="z-[1]"
-          >
-            <div className="flex-row flex mb-5">
-              <Image
-                src="/vip-clinic.png"
-                height={150}
-                width={150}
-                alt="HireSide"
-              />
-              <div className="p-3 ml-3">
-                <p className="text-white font-semibold text-xl">VIP Clinic</p>
-              </div>
-            </div>
-          </Link>
+          {/* ... (unchanged) */}
         </div>
 
-        <div className="flex-col flex md:flex-row mt-7"> {/* Moved down */}
+        <div className="flex-col flex md:flex-row mt-7">
           {/* Animals of Africa */}
-          <Link
-            href="https://github.com/tugrul95/SwiftUI-Animals-of-Africa"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="z-[1]"
-          >
-            <div className="flex-row flex mb-5">
-              <Image
-                src="/animals-of-africa.png"
-                height={150}
-                width={150}
-                alt="VergeSystems"
-              />
-              <div className="p-3 ml-3">
-                <p className="text-white font-semibold text-xl">
-                  Animals of Africa
-                </p>
-              </div>
-            </div>
-          </Link>
+          {/* ... (unchanged) */}
 
-          {/* E-commerce app */}
+          {/* Crypto-App */}
           <Link
-            href="https://my-store-ecommerce.netlify.app/"
+            href="https://github.com/tugrul95/Crypto-App"
             rel="noopener noreferrer"
             target="_blank"
             className="z-[1]"
           >
             <div className="flex-row flex mb-5" style={{ marginLeft: '17px' }}>
               <Image
-                src="/my-store.png"
+                src="/crypto-app.png"
                 height={150}
                 width={150}
                 alt="VergeSystems"
               />
               <div className="p-3">
                 <p className="text-white font-semibold text-xl">
-                  E-commerce app  
+                  Crypto App
                 </p>
               </div>
             </div>
           </Link>
+        </div>
+
+        <div className="flex-col flex md:flex-row mt-7">
+          {/* SwiftUI-Weather-App */}
+          <Link
+            href="https://github.com/tugrul95/SwiftUI-Weather-App"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="z-[1]"
+          >
+            <div className="flex-row flex mb-5" style={{ marginLeft: '17px' }}>
+              <Image
+                src="/weather-app.png"
+                height={150}
+                width={150}
+                alt="VergeSystems"
+              />
+              <div className="p-3">
+                <p className="text-white font-semibold text-xl">
+                  SwiftUI-Weather-App
+                </p>
+              </div>
+            </div>
+          </Link>
+
+          {/* E-commerce app */}
+          {/* ... (unchanged) */}
         </div>
       </div>
     </section>
